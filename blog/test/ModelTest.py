98746812f8a9325b00d06a5d01b6fbb66dbe8a26
Chris Jones <chris@pythonistic.com>
2016-03-03 19:25:45 -0500

    Refactoring model tests. Added Author and AuthorStatus.

--- blog/test/ModelTest.py
+++ blog/test/ModelTest.py
@@ -1,13 +1,12 @@
 import unittest
 
-from sqlalchemy.orm import session
-
 from blog import Session
-
 from blog.Model import *
 
 
 class ModelTest(unittest.TestCase):
+
+    ID = -1
 
     def setUp(self):
         self.session = Session()
@@ -15,23 +14,29 @@ class ModelTest(unittest.TestCase):
     def tearDown(self):
         self.session.close()
 
-    def test_approval(self):
-        ID = -1
+    def clean(self, clz):
+        """
+        Remove the test record for the model class.
 
-        results = self.session.query(Approval).filter(Approval.id == ID).all()
+        clz: The test record class.
+        """
+        results = self.session.query(Approval).filter(clz.id == self.ID).all()
         for result in results:
             self.session.delete(result)
         self.session.commit()
 
+    def test_approval(self):
+        self.clean(Approval)
+
         approval = Approval()
-        approval.id = ID
+        approval.id = self.ID
         approval.status = 'foo'
         approval.description = 'description'
 
         self.session.add(approval)
         self.session.commit()
 
-        loaded_approval = self.session.query(Approval).filter(Approval.id == ID).first()
+        loaded_approval = self.session.query(Approval).filter(Approval.id == self.ID).first()
         self.assertEqual(approval.id, loaded_approval.id)
         self.assertEqual(approval.status, loaded_approval.status)
         self.assertEqual(approval.description, loaded_approval.description)
@@ -39,3 +44,51 @@ class ModelTest(unittest.TestCase):
 
         self.session.delete(loaded_approval)
         self.session.commit()
+
+        self.clean(Approval)
+
+    def test_author(self):
+        self.clean(Author)
+        self.clean(AuthorStatus)
+
+        status = AuthorStatus()
+        status.id = self.ID
+        status.status = 'foo'
+        status.description = 'description'
+        self.session.add(status)
+        self.session.commit()
+
+        loaded_status = self.session.query(AuthorStatus).filter(Author.id == self.ID).first()
+        self.assertEqual(status.id, loaded_status.id)
+        self.assertEqual(status.status, loaded_status.status)
+        self.assertEqual(status.description, loaded_status.description)
+        self.assertEqual(str(status), str(loaded_status))
+
+        author = Author()
+        author.id = self.ID
+        author.login = 'foo'
+        author.password = 'bar'
+        author.email = 'baz'
+        author.url = 'frobble'
+        author.activation_key = 'activation'
+        author.status = loaded_status
+        author.display_name = 'name'
+        self.session.add(author)
+        self.session.commit()
+
+        loaded_author = self.session.query(Author).filter(Author.id == self.ID).first()
+        self.assertEqual(author.id, loaded_author.id)
+        self.assertEqual(author.login, loaded_author.login)
+        self.assertEqual(author.password, loaded_author.password)
+        self.assertEqual(author.email, loaded_author.email)
+        self.assertEqual(author.url, loaded_author.url)
+        self.assertEqual(author.activation_key, loaded_author.activation_key)
+        self.assertEqual(author.status, loaded_author.status)
+        self.assertEqual(author.display_name, loaded_author.display_name)
+        self.assertIsNone(author.created)
+        self.assertIsNotNone(loaded_author.created)
+        self.assertNotEqual(str(author), str(loaded_author))
+
+        self.clean(Author)
+        self.clean(AuthorStatus)
+
